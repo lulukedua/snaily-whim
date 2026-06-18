bfb1a71bd84cc5d6ca6d97f87a072933aba7b4e3
lulultfh <punyalulu05@gmail.com>
2026-06-18 19:59:58 +0700

    feat: menambahkan color

---
+++ lib/core/theme/colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  AppColors._();
+
+  // bg
+  static const Color bgColor = Color(0xffFFF8F0);
+  static const Color bgBtmColor = Color(0xffA5B68D);
+
+  // prim
+  static const Color primColor = Color(0xffC1CFA1);
+
+  // text
+  static const Color primTextColor = Color(0xff540863);
+  static const Color warningTextColor = Color(0xffEB4C4C);
+  static const Color secondaryTextColor = Color(0xff8D5F8C);
+}
